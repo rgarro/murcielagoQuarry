339e1d3bf822b4572c4dabd81b18930ebdfd31a5
rgarro <rgarro@gmail.com>
2026-01-04 10:53:24 -0600

    dance into the fire To fatal sounds of broken dreams ...

--- Assets/Scripts/caterpillarCT660Controller.cs
+++ Assets/Scripts/caterpillarCT660Controller.cs
@@ -1,14 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/**
+ *                          .------.
+ *                         :|||"""`.`.
+ *                         :|||     7.`.
+ *      .===+===+===+===+===||`----L7'-`7`---.._
+ *      []                  || ==       |       """-.
+ *      []...._____.........||........../ _____ ____|
+ *     c\____/,---.\_       ||_________/ /,---.\_  _/
+ *      /_,-/ ,-. \ `._____|__________||/ ,-. \ \_[
+ *          /\ `-' /                    /\ `-' /
+ *            `---'                       `---'         
+ * Vaya al cajerito adrento de GAP, saque la harina, pero le deja la mitad al Piedrero que deja entrar 
+ * Saqueme la planilla le dijo al parse, yo le dono la mitad a su Bar
+ *
+ *
+ * @author Rolando<rgarro@gmail.com>
+ */
 public class caterpillarCT660Controller : MonoBehaviour
 {
+
+    public GameObject leftFrontWheel;
+    public GameObject rightFrontWheel;
+    public GameObject leftCenterWheel;
+    public GameObject rightCenterWheel;
+    public GameObject leftRearWheel;
+    public GameObject rightRearWheel;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    void increaseShiftGear(){}
+
+    void decreaseShiftGear(){}
 
     // Update is called once per frame
     void Update()
