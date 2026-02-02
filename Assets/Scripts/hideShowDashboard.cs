93a06e227600e97cced54748e43a33a1a131c473
rgarro <rgarro@gmail.com>
2026-02-02 11:33:02 -0600

    cosmic supernatural kid loco ..

--- Assets/Scripts/hideShowDashboard.cs
+++ Assets/Scripts/hideShowDashboard.cs
@@ -16,7 +16,24 @@ using UnityEngine;
  *    \%\   /              \%\   /                  \%\   /
  *      ~~~~                 ~~~~                     ~~~~   
  * Shows / Hides Dashboard with Turning Wheel
- *
+ * En los Juegos se vale poner cualquier cosa hijueputa
+ * modificaciones de junta directiva , estupideces de contadores playos
+ * Los LandRover son de Aluminio, el Jeep willys es un fierro , PIENSELO!!
+ * mataron a Raquel la de la UIA , cerraron el Bar Romeral
+ * shackle bracket vicker gun rotacion gravitacional opuesta contra la rotacion del arbol de levas
+ * por eso los helicopteros tienen turbina con varilla de transmision de titanio y clutch de empuje de aire comprimido
+ *  entonces hay que dispararles organismos electronicos que habitan en realidades estratosfericas
+ * un drone MKraptor Mato a Somoza, un drone MKraptor Mato a Somoza
+ * el comandante CERO fue verguiado por drones !!!
+ * El Howitzer del Volcan Barva apunta a los ultras con municion fria que hace una explocion electrica de protones
+ * las explociones de protones pueden pasar hasta inadvertidas, detienen la Vida y el cuerpo fisico se desintegra 
+ * sin reaccion termica
+ * Los Drones de Control Remoto son aviones de contro remoto, no son drones, solo son plataformas posibles
+ * encontrando los canales del receptor e instalando un relay a un android soldado en el dron para que sea una
+ * computadora con un control autonomo es algo que paga por verse hecho igual que secar Cacao ...
+ * la metralleta vicker enfriada con agua montada en un shackle bracket de jeep willys para que la elevacion 
+ * este en un rango de tiempo delta humano y sirva para pegar un DC3 piloteado humano la tenian los calderonistas en 1948;
+ *   TOME CAFE 1820 ....
  *
  *
  * @author Rolando <rgarro@gmail.com> <https://emptyart.github.io>
@@ -45,7 +62,7 @@ public class hideShowDashboard : MonoBehaviour
     void activateDash(){
         
         for (int i = 0; i < this.gauges.Length; i++){
-            Debug.Log("dashboard object "+i);
+            //Debug.Log("dashboard object "+i);
             this.gauges[i].SetActive(true);
         }
         this.dashboard.SetActive(true);
@@ -63,12 +80,12 @@ public class hideShowDashboard : MonoBehaviour
         if(GUI.Button(new Rect(this.buttons_x_corner,this.buttons_y_corner,this.buttonWidth,this.buttonHeight), this.dashboardIcon)) 
         {
                if(this.dashboard_is_on){
-                Debug.Log("dashboard on click ...");
+                //Debug.Log("dashboard on click ...");
                 this.deactivateDash();
                 //this.dashboard.SetActive(false);
                 this.dashboard_is_on = false;
             } else {
-                Debug.Log("dashboard off click ...");
+                //Debug.Log("dashboard off click ...");
                 this.activateDash();
                 //this.dashboard.SetActive(true);
                 this.dashboard_is_on = true;
